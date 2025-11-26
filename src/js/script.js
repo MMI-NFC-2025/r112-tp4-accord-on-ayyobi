1fb841271a9ee922ba793a71c9d9ff3bba15544d
Nargis <125502580+ayyobi@users.noreply.github.com>
2025-11-26 14:34:26 +0100

    css modifier et ajouter js

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
-// *** ACCORDEON ***
+//  ACCORDEON 
+document.querySelector(".accordeon").addEventListener("click", (e) => {
+  const summary = e.target.closest("summary");
+  if (!summary) return;
+
+  const current = summary.parentElement;
+
+  document.querySelectorAll(".accordeon details").forEach((det) => {
+    if (det !== current) det.open = false;
+  });
+});
